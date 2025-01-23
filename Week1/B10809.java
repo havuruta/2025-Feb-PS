05c91b2c75d3b5a6fe28c570188d283016653b2b
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-01-23 13:41:14 +0900

    init: Week1

---
+++ Week1/B10809.java
@@ -0,0 +1,7 @@
+// B10809.java
+// 문제 10809: 알파벳 찾기 - 각 알파벳의 첫 등장 위치를 출력.
+public class B10809 {
+    public static void main(String[] args) {
+        // 문제 10809 해결을 위한 코드를 작성하세요.
+    }
+}
---
+++ Week1/B1546.java
@@ -0,0 +1,7 @@
+// B1546.java
+// 문제 1546: 평균 - 점수를 조작하여 새로운 평균 계산.
+public class B1546 {
+    public static void main(String[] args) {
+        // 문제 1546 해결을 위한 코드를 작성하세요.
+    }
+}
