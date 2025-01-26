d4bf90231151fe6968813471227a597ec549a29c
김규민 <eg1mb@naver.com>
2025-01-26 22:04:24 +0900

    B10809 문제 풀이

---
+++ Week1/gyumin/gyumin/B1978.java
@@ -0,0 +1,8 @@
+package gyumin;
+// B1978.java
+// 문제 1978: 소수 찾기 - 주어진 숫자 중 소수의 개수를 계산.
+public class B1978 {
+    public static void main(String[] args) {
+        // 문제 1978 해결을 위한 코드를 작성하세요.
+    }
+}
---
+++ Week1/gyumin/gyumin/B2609.java
@@ -0,0 +1,8 @@
+package gyumin;
+// B2609.java
+// 문제 2609: 최대공약수와 최소공배수 - 두 자연수의 GCD와 LCM 계산.
+public class B2609 {
+    public static void main(String[] args) {
+        // 문제 2609 해결을 위한 코드를 작성하세요.
+    }
+}
