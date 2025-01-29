cbc612259023033eb783ebe1bc938826593d61ff
김규민 <eg1mb@naver.com>
2025-01-29 19:36:42 +0900

    B11050 풀이

--- Week1/gyumin/gyumin/B11050.java
+++ Week1/gyumin/gyumin/B11050.java
@@ -1,8 +1,49 @@
 package gyumin;
+
+import java.util.Scanner;
+
 // B11050.java
 // 문제 11050: 이항 계수 1 - 주어진 N과 K로 이항 계수 계산.
 public class B11050 {
     public static void main(String[] args) {
         // 문제 11050 해결을 위한 코드를 작성하세요.
+    	
+    	/*
+    	 * 풀이에 필요한 개념: 조합, 이항계수
+    	 * 1) 조합
+    	 * : n개의 선택지 중, r개를 무작위로 뽑은 경우의 수
+    	 * 
+    	 * 2) 이항게수
+    	 * : 조합의 개념을 이용하여 이항정리를 하는 내용
+    	 * (a+b) ** n 을 생각할 때 각 항은 a를 r개 b를 n-r개 고르는 경우에 해당함
+    	 * 이 개념을 이용하면 r을 0부터 n까지 날리는 시그마를 활용해 각 항의 계수를 구할 수 있음
+    	 */
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	
+    	int r = sc.nextInt();
+    	
+    	// n!
+    	int npac = 1;
+    	for(int i = n; i > 0; i--) {
+    		npac *= i;
+    	}
+    	
+    	// r!
+    	int rpac = 1;
+    	for(int i = r; i > 0; i--) {
+    		rpac *= i;
+    	}
+    	
+    	//(n-r)!
+    	int nMr = 1;
+    	for(int i = (n-r); i > 0; i--) {
+    		nMr *= i;
+    	}
+    	
+    	// 연산
+    	System.out.println(npac / (rpac * nMr));
     }
 }
