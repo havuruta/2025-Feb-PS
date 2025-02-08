3bba5d17b96a71755de28a56177f9ccae4882950
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-02-09 06:42:08 +0900

    Update B10828.java

--- Week1/problem/B10828.java
+++ Week1/problem/B10828.java
@@ -1,8 +1,49 @@
 package problem;
-// B10828.java
-// 문제 10828: 스택 - 정수를 저장하는 스택의 다양한 연산 구현.
+
+/*
+ 문제 : 10845(스택)
+ 시간 : 460ms
+ 풀이 : 문제에서 요구하는 명령어에 맞는 스택 제공 메서드 활용.
+ 입력 형식에 맞는 조건문 분기 설정 및 스택이 비어있는 경우 고려가 필요.
+ */
+
+import java.util.Scanner;
+import java.util.Stack;
+
 public class B10828 {
     public static void main(String[] args) {
-        // 문제 10828 해결을 위한 코드를 작성하세요.
-    }
+        Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		Stack<Integer> st = new Stack<>();
+		
+		for (int i = 0; i < N; i++) {
+			String order = sc.next();
+			
+			char c = order.charAt(0);
+			if (c == 'p') { // push, pop
+				char c2 = order.charAt(1);
+				
+				if (c2 == 'u') { // push
+					int val = sc.nextInt();
+					st.push(val);
+				}
+				else { // pop
+					if(st.empty()) System.out.println(-1);
+					else System.out.println(st.pop());
+				}
+			}
+			else if (c == 't') { // top
+				if(st.empty()) System.out.println(-1);
+				else System.out.println(st.peek());
+			}
+			else if (c == 's') { // size
+				System.out.println(st.size());
+			}
+			else { // empty
+				if(st.empty()) System.out.println(1);
+				else System.out.println(0);
+			}
+		}
+	}
 }
