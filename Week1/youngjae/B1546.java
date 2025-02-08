ad9edd30ec5f8e9f4f25c6355af06979b64f2e95
SsongYJ <97214330+SsongYJ61@users.noreply.github.com>
2025-02-09 02:04:25 +0900

    week1

--- Week1/youngjae/B1546.java
+++ Week1/youngjae/B1546.java
@@ -1,8 +1,31 @@
 package youngjae;
+
+import java.util.Scanner;
+
 // B1546.java
 // 문제 1546: 평균 - 점수를 조작하여 새로운 평균 계산.
 public class B1546 {
     public static void main(String[] args) {
         // 문제 1546 해결을 위한 코드를 작성하세요.
+        Scanner sc = new Scanner(System.in);
+
+		float max = -1f;
+		int majorNum = sc.nextInt();
+		float sum = 0;
+		float result = 0f;
+		int[] arr = new int[majorNum];
+
+		for (int i = 0; i < majorNum; i++) {
+			int score = sc.nextInt();
+			arr[i] = score;
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		for (int k = 0; k < arr.length; k++) {
+			sum +=  (float)arr[k] / max * 100;
+		}
+		result = sum / (float)majorNum;
+		System.out.println(result);
     }
 }
--- Week1/youngjae/B1978.java
+++ Week1/youngjae/B1978.java
@@ -1,8 +1,32 @@
 package youngjae;
+
+import java.util.Scanner;
+
 // B1978.java
 // 문제 1978: 소수 찾기 - 주어진 숫자 중 소수의 개수를 계산.
 public class B1978 {
     public static void main(String[] args) {
         // 문제 1978 해결을 위한 코드를 작성하세요.
+        Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int result = 0;
+		
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int count = 0;
+			for (int j = 1; j <= a; j++) {
+				if (a != 1 && a % j == 0) {
+					count += 1;
+					
+				}
+			}
+            if (count == 2) {
+						result += 1;
+			}
+		}
+		System.out.println(result);
     }
+
 }
