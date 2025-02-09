807055b1eba94d212920f6ec4d896c6d0ddba557
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-02-09 15:19:07 +0900

    init

---
+++ Week1/problem/B11050.java
@@ -0,0 +1,8 @@
+package problem;
+// B11050.java
+// 문제 11050: 이항 계수 1 - 주어진 N과 K로 이항 계수 계산.
+public class B11050 {
+    public static void main(String[] args) {
+        // 문제 11050 해결을 위한 코드를 작성하세요.
+    }
+}
