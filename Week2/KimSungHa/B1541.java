757e2e852ba5fc9b6f5d1a5d42c48e43b51b2e07
CarlosKim-1997 <kimsh5491@naver.com>
2025-02-14 08:53:16 +0900

    [Week2] Add Solutions

---
+++ Week2/KimSungHa/B1541.java
@@ -0,0 +1,7 @@
+package problem;
+
+public class B1541 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Week2/KimSungHa/B2559.java
@@ -0,0 +1,7 @@
+package problem;
+
+public class B2559 {
+    public static void main(String[] args) {
+        
+    }
+}
