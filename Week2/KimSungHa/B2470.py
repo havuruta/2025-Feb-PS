2473139b7b8669a17f4813c7b6b1e381206918c4
CarlosKim-1997 <kimsh5491@naver.com>
2025-02-14 17:43:30 +0900

    [Week2] KimSungHa

--- Week2/KimSungHa/B2470.py
+++ Week2/KimSungHa/B2470.py
@@ -1,7 +1,22 @@
-package problem;
+# 문제: 2470번 두 용액
+# 설명: 양과 음의 정수로 이뤄진 값들 중 두 값의 합이 0과 가장 가까운 경우를 찾아라
 
-public class B2470 {
-    public static void main(String[] args) {
-        
-    }
-}
+N = int(input())
+solutions = list(map(int,input().split()))
+
+solutions.sort() # 크기 순으로 정렬
+
+min1 = 0
+min2 = 0
+min_gap = abs(solutions[0]+solutions[1]) # 초기값 설정
+
+for i in range(N):
+    for j in range(i+1,N):  # 조합 찾기
+        if min_gap>abs(solutions[i]+solutions[j]): # 초기값보다 현재의 두 쌍으로 만든 조합이 0과 더 가까우면
+            min_gap = abs(solutions[i]+solutions[j])    # 대체 후 각 변수에 등록
+            min1 = solutions[i]
+            min2 = solutions[j]
+
+print(min1, min2)
+
+# 자꾸 시간 초과... 좀 더 효율적인 방법을 찾아야 하나
