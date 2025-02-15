ea44db5f3aff405eac28719843219bf050167ed4
김규민 <eg1mb@naver.com>
2025-02-15 23:08:13 +0900

    revert가 정상 동작하지 않아 재시도

--- Week2/gyumin/B10820.java
+++
@@ -1,9 +0,0 @@
-package gyumin;
-
-import java.io.IOException;
-
-public class B10820 {
-    public static void main(String[] args) throws IOException{
-        System.out.println("됨?");
-    }
-}
--- Week2/gyumin/B1259.java
+++
@@ -1,9 +0,0 @@
-package gyumin;
-
-import java.io.IOException;
-
-public class B1259 {
-    public static void main(String[] args) throws IOException{
-        
-    }
-}
