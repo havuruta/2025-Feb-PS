246f40972533b6da1d89d2882248f528aceede70
김희망 <gmlakd990514@naver.com>
2025-03-02 00:02:35 +0900

    [Week4] Add B1977 Solutions

--- Week4/huimang/B1977.java
+++ Week4/huimang/B1977.java
@@ -1,9 +1,41 @@
 package Week4.problem;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class B1977 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+
+        // 완전 제곱수를 모두 찾아 합을 출력해야하니..
+        int sum = 0;
+
+        //그리고 완전 제곱수 중 최소값
+        int min = Integer.MAX_VALUE;
+        for(int i = N; i<=M; i++) {
+
+            // i가 완전 제곱수인지.. 1부터 찾아줌.
+            for(int j=1; j<=i; j++) {
+                if(j * j == i) {
+                    // 완전 제곱수면 더함
+                    sum += i;
+                    if(min > i) {
+                        min = i;
+                    } 
+                }
+            }
+        }
+
+        if(sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
     }
 }
