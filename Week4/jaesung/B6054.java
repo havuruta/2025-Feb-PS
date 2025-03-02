5de8d8954438642a81408fbcfba6c81d04c52aa6
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-03-02 12:24:25 +0900

    B6054

---
+++ Week4/jaesung/B6054.java
@@ -0,0 +1,44 @@
+package Week4.problem;
+
+/*
+ 문제 : 1145(적어도 대부분의 배수)
+ 시간 : 200ms
+ 풀이 : 입력 배열에서 가장 작은 값을 1씩 증가시키며 적어도 대부분의 배수인지 판별
+ */
+
+import java.util.Scanner;
+
+public class B6054 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[5];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		// 가장 작은 값을 1씩 증가시키며 적어도 대부분의 배수 도출
+		int val = arr[0];
+		while (true) {
+			int count = 0;
+
+			for (int j = 0; j < arr.length; j++) {
+				// 배수 확인
+				if (val % arr[j] == 0) {
+					++count;
+				}
+			}
+
+			// 최소값 출력
+			if (count >= 3) {
+				System.out.println(val);
+				return;	
+			}
+			
+			++val;
+		}
+	}
+}
